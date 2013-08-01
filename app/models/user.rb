7c06e12c5bbd4473c191c057fe697dc3f275fcbf
Reid Pearson <rapearso@gmail.com>
2013-08-01 11:23:36 -0700

    Fix Local Host Syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable. #:recoverable, 
   # attr_accessible :title, :body
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name, :as => [:default, :admin]
