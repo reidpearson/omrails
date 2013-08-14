ab8eb0442283ec68280634944dfaa80a054f0773
Reid Pearson <rapearso@gmail.com>
2013-08-13 21:32:34 -0700

    Test update add user id migration

--- db/migrate/20130802180529_add_user_id_to_pins.rb
+++ db/migrate/20130802180529_add_user_id_to_pins.rb
@@ -1,6 +1,17 @@
+#class AddUserIdToPins < ActiveRecord::Migration
+ # def change
+  #  add_column :pins, :user_id, :integer
+   # add_index :pins, :user_id
+  #end
+#end
+
+
 class AddUserIdToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :user_id, :integer
+    create_table :pins do |t|
+      t.integer :user_id
+      t.timestamps
+    end
     add_index :pins, :user_id
   end
 end
