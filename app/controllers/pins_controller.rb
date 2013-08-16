ef4206eac823e87ee184f9b88033dbec85e6a766
Reid Pearson <rapearso@gmail.com>
2013-08-15 23:13:17 -0700

    Add Pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,7 +7,7 @@ class PinsController < ApplicationController
   #end
 
   def index
-    @pins = Pin.order ("created_at desc")
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
