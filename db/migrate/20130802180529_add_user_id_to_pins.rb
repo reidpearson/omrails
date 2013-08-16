47fc5d0e0a8b337f394d98501d2f3c078ef2a8d7
Reid Pearson <rapearso@gmail.com>
2013-08-15 23:52:39 -0700

    Update Add User ID to pins migration to fix heroku

--- db/migrate/20130802180529_add_user_id_to_pins.rb
+++ db/migrate/20130802180529_add_user_id_to_pins.rb
@@ -1,17 +1,17 @@
-#class AddUserIdToPins < ActiveRecord::Migration
- # def change
-  #  add_column :pins, :user_id, :integer
-   # add_index :pins, :user_id
-  #end
-#end
-
-
 class AddUserIdToPins < ActiveRecord::Migration
-  def change
-    create_table :pins do |t|
-      t.integer :user_id
-      t.timestamps
-    end
+ def change
+   add_column :pins, :user_id, :integer
     add_index :pins, :user_id
   end
 end
+
+
+#class AddUserIdToPins < ActiveRecord::Migration
+ # def change
+  #  create_table :pins do |t|
+   #   t.integer :user_id
+    #  t.timestamps
+    #end
+    #add_index :pins, :user_id
+  #end
+#end
